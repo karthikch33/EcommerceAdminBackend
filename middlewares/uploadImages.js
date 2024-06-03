51799a8924159704db09ae3a5e4a8c90c322912c
Karthik Ch <saipavan39dh@gmail.com>
2024-06-03 16:12:26 +0530

    filePractice uploading images and resizing

--- middlewares/uploadImages.js
+++ middlewares/uploadImages.js
@@ -8,6 +8,82 @@ import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+
+
+
+
+
+
+
+
+
+
+
+/*
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+const __dirname = dirname(__filename)
+
+
+const store = multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,path.join(__dirname),'../images/photos')
+    },
+    filename:function(req,file,cb){
+        const unqname = Date.now() + '-' + Math.round(Math.random() * 1e9)
+        cb(null,file.fieldname + '-' + unqname + '.jpeg')
+    }
+})
+
+const filter = (req,file,cb)=>{
+    if(file.mimetype.startsWith('image')){
+        cb(null,true)
+    }
+}
+
+
+const uploadImg = multer({
+    storage: storedisk,
+    filter:filter,
+    limits:{fieldsize:2000000}
+})
+
+const resizeImage = (req,res,next)=>{
+    if(!req.files) next()
+    await Promise.all(
+        req.files.map((file)=>{
+            sharp(file.filename).resize(300,300).toFormat('jpeg').jpeg({quality:90}).file('filepathdesitnatinowithfilename')
+        })
+    )
+}
+
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__dirname)
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const multerStorage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, path.join(__dirname, '../public/images'));
@@ -27,6 +103,52 @@ const multerFilter = (req,file,cb)=>{
         },false)
     }
 }
+
+/* 
+photo uploaded path to store 
+
+first try to store in the disk create a folder for storing images 
+
+const diskstore = multer.diskStorage({
+    destination: function(req,file,cb){
+        cb(null,path.join(__dirname,'../public/images'))
+    }
+})
+
+
+next try to create a unq name for the file to store in the storage space you stored
+
+const diskstore = multer.diskStorage({
+    destination: function(req,file,cb){
+        cb(null,path.join(__dirname , '..public/images'))
+    },
+    filename:function(req,file,cb){
+        const unqname = Date.now() + '-' + Math.round(Math.random() * 1e9)
+        cb(null,file.fieldname + '-' + unqname + '.jpeg')
+    }
+})
+
+next try to filter those files only images 
+
+const fileterimages = (req,file,cb)=>{
+    if(file.mimetype.startWith('images')){
+        cb(null,true)
+    }
+    else{
+        cb({
+            message:"File not supported"
+        },false)
+    }
+}
+
+set the size limit for the file 
+const uploadphoto = multer({
+    storage:diskstore,
+    filter:filterimages,
+    limits:{fieldSize:200000000}
+})
+
+*/
 
 const uploadPhoto = multer({
     storage:multerStorage,
